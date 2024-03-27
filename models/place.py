f27ed9a9ae99ee8eb1fef5dcf503d6605f67be38
Michael Solomon <michaelelsa12@gmail.com>
2024-03-28 00:41:22 +0300

    home_page&tests

--- models/place.py
+++ models/place.py
@@ -4,7 +4,7 @@ import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
-from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table, ARRAY
 from sqlalchemy.orm import relationship
 
 
