054cbcd5f4047a4cf6e8d5c8e148226d35acbc31
Michael Solomon <michaelelsa12@gmail.com>
2024-03-30 14:48:58 +0300

    catagory end point

---
+++ api/v1/views/catagories.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python3
+""" objects that handles all default RestFul API actions for Catagories"""
+from models.catagory import Catagory
+from models import storage
+from api.v1.views import app_views
+from flask import abort, jsonify, make_response, request
+from flasgger.utils import swag_from
+
+
+@app_views.route('/catagory', methods=['GET'], strict_slashes=False)
+@swag_from('documentation/catagory/all_catagories.yml')
+def get_users():
+    """
+    Retrieves all list of Catagory in  a system
+    """
+    all_catagory = storage.all(Catagory).values()
+    list_catagories = []
+    for cata in all_catagory:
+        list_catagories.append(cata.to_dict())
+    return jsonify(list_catagories)
+
+
+@app_views.route('/catagory/<catagory_id>/', methods=['GET'],
+                 strict_slashes=False)
+@swag_from('documentation/user/get_user.yml', methods=['GET'])
+def get_amenity(catagory_id):
+    """ Retrieves an catagory based on id """
+    cata = storage.get(Catagory, catagory_id)
+    if not cata:
+        abort(404)
+
+    return jsonify(cata.to_dict())
+
+
+
+@app_views.route('/catagory/<catagory_id>', methods=['DELETE'],
+                 strict_slashes=False)
+@swag_from('documentation/catagory/delete_catagory.yml', methods=['DELETE'])
+def delete_amenity(user_id):
+    """
+    Deletes an Catagory  Object
+    """
+
+    cata = storage.get(Catagory, user_id)
+
+    if not cata:
+        abort(404)
+
+    storage.delete(cata)
+    storage.save()
+
+    return make_response(jsonify({}), 200)
+
+
+@app_views.route('/catagory', methods=['POST'], strict_slashes=False)
+@swag_from('documentation/catagory/post_catagory.yml', methods=['POST'])
+def post_state():
+    """
+    Creates a Catagory
+    """
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+
+    if 'name' not in request.get_json():
+        abort(400, description="Missing name")
+
+    data = request.get_json()
+    instance = Catagory(**data)
+    instance.save()
+    return make_response(jsonify(instance.to_dict()), 201)
+
+
+@app_views.route('/catagory/<catagory_id>', methods=['PUT'], strict_slashes=False)
+@swag_from('documentation/catagory/put_catagory.yml', methods=['PUT'])
+def put_state(catagory_id):
+    """
+    Updates a Catgory
+    """
+    cata = storage.get(Catagory, catagory_id)
+
+    if not cata:
+        abort(404)
+
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+
+    ignore = ['id', 'created_at', 'updated_at']
+
+    data = request.get_json()
+    for key, value in data.items():
+        if key not in ignore:
+            setattr(cata, key, value)
+    storage.save()
+    return make_response(jsonify(cata.to_dict()), 200)
+
+
+
+
+
+
